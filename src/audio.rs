220d06ee10d8b00a6e011e2635d42f60bc6d7681
James Sizeland <jamessizeland@gmail.com>
2023-04-09 18:14:50 +0100

    update audio assets to include more sounds

--- src/audio.rs
+++ src/audio.rs
@@ -25,24 +25,34 @@ impl Plugin for InternalAudioPlugin {
 }
 
 #[derive(Resource)]
-struct FlyingAudio(Handle<AudioInstance>);
+struct AudioInstances {
+    flying: Handle<AudioInstance>,
+    shooting: Handle<AudioInstance>,
+    bouncing: Handle<AudioInstance>,
+}
 
 fn start_audio(mut commands: Commands, audio_assets: Res<AudioAssets>, audio: Res<Audio>) {
     audio.pause();
-    let handle = audio
-        .play(audio_assets.flying.clone())
-        .looped()
-        .with_volume(0.3)
-        .handle();
-    commands.insert_resource(FlyingAudio(handle));
+    fn gen_instance(audio: &Res<Audio>, handle: &Handle<AudioSource>) -> Handle<AudioInstance> {
+        audio
+            .play(handle.clone())
+            .looped()
+            .with_volume(0.3)
+            .handle()
+    }
+    commands.insert_resource(AudioInstances {
+        flying: gen_instance(&audio, &audio_assets.flying),
+        shooting: gen_instance(&audio, &audio_assets.shoot),
+        bouncing: gen_instance(&audio, &audio_assets.force_field_3),
+    });
 }
 
 fn control_flying_sound(
     actions: Res<Actions>,
-    audio: Res<FlyingAudio>,
+    audio: Res<AudioInstances>,
     mut audio_instances: ResMut<Assets<AudioInstance>>,
 ) {
-    if let Some(instance) = audio_instances.get_mut(&audio.0) {
+    if let Some(instance) = audio_instances.get_mut(&audio.flying) {
         match instance.state() {
             PlaybackState::Paused { .. } => {
                 if actions.player_movement.is_some() {
